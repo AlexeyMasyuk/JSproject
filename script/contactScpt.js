f941c47918130187572df9a6948f250a6f80fe63
unknown <alexeymasyuk@gmail.comm>
2020-06-12 09:41:40 +0300

    12.6

--- script/contactScpt.js
+++ script/contactScpt.js
@@ -1,17 +1,24 @@
 let contact = {
     "email": "",
     "userName": "",
-    "subject": ""
+    "subject": "",
+    "text": ""
 };
 
 function myFunction() {
-
-    document.getElementById("usrform").submit();
-    document.getElementById("usrform").style.display = "none";
-    contact["email"] = document.getElementById("usrform").elements[0].value;
-    contact["userName"] = localStorage.getItem("person");
-    contact["subject"] = document.getElementById("usrform").elements[1].value;
-    alert(contact["subject"]);
-    alert(contact["email"]);
-    alert(contact["userName"]);
+    if (localStorage.getItem("person") != null) {
+        contact["email"] = document.getElementById("usrform").elements[0].value;
+        contact["userName"] = localStorage.getItem("person");
+        contact["subject"] = document.getElementById("usrform").elements[1].value;
+        contact["text"] = document.getElementById("usrform").elements[2].value;
+        if (contact.email.length > 0 && contact.userName.length > 0 && contact.subject.length > 0) {
+            document.write("User Email: " + contact.email + '<br>');
+            document.write("UserName: " + contact.userName + '<br>');
+            document.write("Subject: " + contact.subject + '<br>');
+            document.write(contact.text);
+        } else {
+            alert("Fill all fields");
+        }
+    } else
+        alert("Return to main page and fill your name");
 }
